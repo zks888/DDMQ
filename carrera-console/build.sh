aaf645dad23fa948a42bebcf2efab47c732615ab
zhaokaishan <278506089@qq.com>
2019-09-06 21:31:05 +0800

    使用宿主机的MYSQL

--- carrera-console/build.sh
+++ carrera-console/build.sh
@@ -4,13 +4,6 @@ BASEDIR=$(dirname "$0")
 cd ${BASEDIR}
 WKDIR=`pwd`
 
-# cd ${BASEDIR}/carrera-console-fe
-# sh build.sh
-# cd ..
-# rm -rf carrera-console/src/main/webapp/build carrera-console/src/main/webapp/static carrera-console/src/main/webapp/index.html
-# cp -R carrera-console-fe/dist/* carrera-console/src/main/webapp
-
-
 cd ${BASEDIR}/carrera-console
 
 mvn clean package -Pdev -Dmaven.test.skip=true
--- carrera-docker/play-ddmq.sh
+++ carrera-docker/play-ddmq.sh
@@ -1,7 +1,7 @@
 # build mysql image
-if [[ "$(docker images -q mysql:1.0 2> /dev/null)" == "" ]]; then
-  docker build -t mysql:1.0 ./mysql
-fi
+# if [[ "$(docker images -q mysql:1.0 2> /dev/null)" == "" ]]; then
+#   docker build -t mysql:1.0 ./mysql
+# fi
 
 # build ddmq image
 if [[ "$(docker images -q ddmq:1.0 2> /dev/null)" == "" ]]; then
@@ -9,9 +9,9 @@ if [[ "$(docker images -q ddmq:1.0 2> /dev/null)" == "" ]]; then
 fi
 
 # run mysql container
-echo 'start mysql container...'
-docker run -d --rm --name mysql -p 127.0.0.1:3306:3306 -t mysql:1.0
+# echo 'start mysql container...'
+# docker run -d --rm --name mysql -p 127.0.0.1:3306:3306 -t mysql:1.0
 
 # run ddmq container
 echo 'start ddmq container.'
-docker run -i --rm -t -p 127.0.0.1:8080:8080 -p 127.0.0.1:9613:9613 -p 127.0.0.1:9713:9713 --name ddmq --link mysql ddmq:1.0
+docker run -i --rm -t -p 127.0.0.1:8080:8080 -p 127.0.0.1:9613:9613 -p 127.0.0.1:9713:9713 --name ddmq ddmq:1.0

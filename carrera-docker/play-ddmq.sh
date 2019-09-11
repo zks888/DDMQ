697d1a17c08f28976e5e946042c1cce4116da567
zhaokaishan <278506089@qq.com>
2019-09-11 09:39:04 +0800

    构建消费端SDK

--- carrera-docker/play-ddmq.sh
+++ carrera-docker/play-ddmq.sh
@@ -10,8 +10,8 @@ fi
 
 # run mysql container
 echo 'start mysql container...'
-docker run -d --rm --name mysql -p 127.0.0.1:3306:3307 -t mysql:1.0
+docker run -d --rm --name mysql -p 127.0.0.1:3307:3306 -t mysql:1.0
 
 # run ddmq container
 echo 'start ddmq container.'
-docker run -i --rm -t -p 127.0.0.1:8080:8080 -p 127.0.0.1:9613:9613 -p 127.0.0.1:9713:9713 --name ddmq --link mysql ddmq:1.0
+docker run -i --rm -t -v /usr/local/var/rocketmq/store:/root/store -p 127.0.0.1:8080:8080 -p 127.0.0.1:9613:9613 -p 127.0.0.1:9713:9713 --name ddmq --link mysql ddmq:1.0

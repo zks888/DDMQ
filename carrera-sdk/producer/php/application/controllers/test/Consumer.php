5d35a6fdce82dce56fc56e1922443da5f979dddf
zhaokaishan <278506089@qq.com>
2019-09-11 14:58:29 +0800

    构建消费端SDK

--- carrera-sdk/producer/php/application/controllers/test/Consumer.php
+++ carrera-sdk/producer/php/application/controllers/test/Consumer.php
@@ -9,30 +9,6 @@ class Consumer extends CI_Controller {
         echo "Hello Carrera Consumer\n";
     }
 
-    public function pull() {
-        $this->load->library('carrera/ThriftConsumer');
-        $ret = $this->thriftconsumer->pull('cg_1', 'tp1');
-        var_dump($ret);
-    }
-
-    public function fetch() {
-        $this->load->library('carrera/ThriftConsumer');
-        $ret = $this->thriftconsumer->fetch('cg_1', 'dc76c321ca5a36e8c94dd58313e2321b');
-        var_dump($ret);
-    }
-
-    public function ack() {
-        $this->load->library('carrera/ThriftConsumer');
-        $ret = $this->thriftconsumer->pull('cg_1', 'tp1');
-        var_dump($ret);
-        if ($ret['code'] > 0) {
-            return;
-        }
-        $context = $ret['ret']['context'];
-        $ret = $this->thriftconsumer->ack('cg_1', $context['']);
-        var_dump($ret);
-    }
-
     public function consume() {
         $this->load->library('carrera/ThriftConsumer');
         $ret = $this->thriftconsumer->pull('cg_1', 'tp1');
